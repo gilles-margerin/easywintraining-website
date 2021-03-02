2f0a0004f5ba118538b594f58c5a26e62ab7002f
Gilles Margerin <gillesmargerin@gmail.com>
2021-03-02 10:51:49 +0100

    icons credit attribution

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,14 +3,7 @@ import styles from './Footer.module.scss'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{" "}
-        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-      </a>
+      <div>Icons made by <a href="https://www.freepik.com" title="Freepik" target="_blank" rel="noopener noreferrer">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel="noopener noreferrer">www.flaticon.com</a></div>
     </footer>
   );
 };

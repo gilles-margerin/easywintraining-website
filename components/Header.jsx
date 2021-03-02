8a754563ba30a5a54ca9ccd744bf1fbb39cb0197
Gilles Margerin <gillesmargerin@gmail.com>
2021-03-02 12:20:09 +0100

    header structure

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,28 +1,66 @@
-import Link from 'next/link'
-import styles from './Header.module.scss'
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
     <nav className={styles.navbar}>
+      <h2>Easywintraining Games</h2>
       <ul>
         <li>
           <Link href="/">
-            <a>Accueil</a>
+            <a>
+              <Image
+                src="/home.svg"
+                alt="homepage icon"
+                width={30}
+                height={30}
+              />
+            </a>
           </Link>
         </li>
         <li>
+          <a
+            href="https://www.facebook.com/Easywintraining-Games-1929362253988650/?ref=page_internal"
+            target="_blank"
+            rel="noopener 
+            noreferrer"
+          >
+            <Image
+              src="/facebook.svg"
+              alt="facebook icon"
+              width={30}
+              height={30}
+            />
+          </a>
+        </li>
+        <li>
           <Link href="/contact">
-            <a>Contact</a>
+            <a>
+              <Image
+                src="/contact.svg"
+                alt="contact icon"
+                width={30}
+                height={30}
+              />
+            </a>
           </Link>
         </li>
         <li>
           <Link href="/timetable">
-            <a>Calendrier</a>
+            <a>
+              <Image
+                src="/calendar.svg"
+                alt="calendar icon"
+                width={30}
+                height={30}
+              />
+            </a>
           </Link>
         </li>
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

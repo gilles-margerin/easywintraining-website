ca9a92de0fd8f72aab143f43d3da2ee6e058aafa
Gilles Margerin <gillesmargerin@gmail.com>
2021-03-10 16:12:01 +0100

    homepage media queries

--- components/Aside.jsx
+++ components/Aside.jsx
@@ -4,28 +4,30 @@ import GoogleMap from './GoogleMap'
 const Aside = ({ apiKey }) => {
   return (
     <aside className={styles.aside}>
-      <h3>
-        Easywintraining Games
-      </h3>
-
-      <address>
-        <a href="tel:+33-6-63-96-36-63">
-          06.63.96.36.63
-        </a>
-        <br/>
-        <a href="mailto:easywintraining@gmail.com">
-          easywintraining@gmail.com 
-        </a>
-        <br/>
-        25 rue de la lanterne 
-        <br/>
-        66000 Perpignan
-      </address>
-
-      <p>
-        Association de jeux de société et d'activités ludiques
-      </p>
-
+      <div>
+        <h3>
+          Easywintraining Games
+        </h3>
+        
+        <address>
+          <a href="tel:+33-6-63-96-36-63">
+            06.63.96.36.63
+          </a>
+          <br/>
+          <a href="mailto:easywintraining@gmail.com">
+            easywintraining@gmail.com 
+          </a>
+          <br/>
+          25 rue de la lanterne 
+          <br/>
+          66000 Perpignan
+        </address>
+        
+        <p>
+          Association de jeux de société et d'activités ludiques
+        </p>
+        
+      </div>
       <GoogleMap apiKey={apiKey} />
 
     </aside>

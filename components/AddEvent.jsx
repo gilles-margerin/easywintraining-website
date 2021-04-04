9f1721f8237b80c02cc9d49376da5d1ac3207a3c
Gilles Margerin <gillesmargerin@gmail.com>
2021-04-04 21:26:50 +0200

    form div display fix

--- components/AddEvent.jsx
+++ components/AddEvent.jsx
@@ -3,27 +3,27 @@ import styles from "./modules/AddEvent.module.scss"
 const AddEvent = () => {
   return (
     <form className={styles.form} action="https://easywintraining-api.herokuapp.com/api/addevent" method="POST">
-      <div>
+      <div className={styles.formDiv}>
         <label htmlFor="eventDate">Date</label>
         <input type="date" id="eventDate" name="eventDate" required/>
       </div>
-      <div>
+      <div className={styles.formDiv}>
         <label htmlFor="eventTime">Horaires</label>
         <input type="text" name="eventTime" id="eventTime" required/>
       </div>
-      <div>
+      <div className={styles.formDiv}>
         <label htmlFor="eventName">Nom de l'activité</label>
         <input type="text" id="eventName" name="eventName" required/>
       </div>
-      <div>
+      <div className={styles.formDiv}>
         <label htmlFor="eventPlace">Lieu de l'activité</label>
         <input type="text" id="eventPlace" name="eventPlace" required/>
       </div>
-      <div>
+      <div className={styles.formDiv}>
         <label htmlFor="eventDescription">Description</label>
         <textarea id="eventDescription" name="eventDescription" required></textarea>
       </div>
-      <div>
+      <div className={styles.formDiv}>
         <label htmlFor="eventType">Type d'activité</label>
         <select name="eventType" id="eventType" required>
           <option>Animations ludiques</option>

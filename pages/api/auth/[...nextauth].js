a758024baf917786c2436ea2b411db4166ae5541
Gilles Margerin <gillesmargerin@gmail.com>
2021-05-31 19:09:08 +0200

    deleted facebook oauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,10 +7,6 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    Providers.Facebook({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET,
-    }),
   ],
 
   session: {

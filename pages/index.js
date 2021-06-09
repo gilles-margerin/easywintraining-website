09e89005c04f347d931180d1796427f9a3b00030
Gilles Margerin <gillesmargerin@gmail.com>
2021-06-09 09:47:58 +0200

    added og meta tags in index

--- pages/index.js
+++ pages/index.js
@@ -35,6 +35,10 @@ export default function Home({ apiKey }) {
       <Head>
         <title>Easywintraining Games - Accueil</title>
         <meta name="description" content="Easywintraining Games est une association de jeux et d'animations ludiques à Perpignan.  Venez vous amuser avec nous et notre vaste choix d'activités!"/>
+        <meta property="og:title" content="Easywintraining Games - Accueil" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://postimg.cc/68vpRkbk" />
+        <meta property="og:url" content="https://easywintraining-games.fr" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Mulish&family=Philosopher:wght@700&display=swap"
